69fb930a98c203a7838cdcb9d61780a9b45f29dd
Andrea S Barr <andreabarr87@gmail.com>
2019-11-04 03:00:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
+array = [2, 4, 6, 8]
+counter = 0
+
 def square_array(array)
-  # your code here
+  while counter < array.length do
+    puts array[counter]**2
+    counter += 1
+  end
 end
+
+square_array(array)
